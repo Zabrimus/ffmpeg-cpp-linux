4e3f42daab4a900dc717e0a9caa988fdd0072437
galla15 <gallacchi@gmail.com>
2022-11-25 12:32:28 +0100

    Add libav dependencies to config
    Signed-off-by: galla15 <gallacchi@gmail.com>

--- ffmpegcpp-config.cmake
+++ ffmpegcpp-config.cmake
@@ -3,7 +3,15 @@ include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/ffmpegcpp.cmake)
 
 get_filename_component(FFMPEGCPP_CONFIG_PATH "${CMAKE_CURRENT_LIST_DIR}" REALPATH)
 
-set(FFMPEGCPP_INCLUDE_DIRS  "${FFMPEGCPP_CONFIG_PATH}/../../include/ffmpegcpp")
-set(FFMPEGCPP_LIBS ffmpegcpp)
+find_package(PkgConfig)
+pkg_check_modules(LIBAV REQUIRED IMPORTED_TARGET 
+                    libavcodec
+                    libavformat
+                    libavutil
+                    libavdevice
+                    libavfilter )
 
-message(-- " FFMPEGCPP found: version ${FFMPEGCPP_VERSION}")
+set(FFMPEGCPP_INCLUDE_DIRS  "${FFMPEGCPP_CONFIG_PATH}/../../include/ffmpegcpp")
+set(FFMPEGCPP_LIBS ffmpegcpp PkgConfig::LIBAV)
+
+message(STATUS "-- Found FFMPEGCPP: version ${FFMPEGCPP_VERSION}")
